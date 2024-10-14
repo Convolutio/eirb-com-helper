ab8c0679fd99e81c7f763a0efd0588bfbe8c9227
Nemo D'ACREMONT <nemo.d'acremont@enseirb-matmeca.fr>
2024-10-14 11:20:24 +0200

    feat: add DEBUG option in .env

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from eirb_com_helper.server import app
 
 HOST = getenv("HOST", "0.0.0.0")
 PORT = int(getenv("PORT", 5000))
+IS_DEBUG = getenv("MODE", "PRODUCTION") == "DEBUG"
 
 if __name__ == "__main__":
-    app.run(debug=True, host=HOST, port=PORT)
+    app.run(debug=IS_DEBUG, host=HOST, port=PORT)
