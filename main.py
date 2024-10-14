78a8973470c22ebf740238eb12cbb271e7f97470
Nemo D'ACREMONT <nemo.d'acremont@enseirb-matmeca.fr>
2024-10-14 10:19:59 +0200

    feat: support HOST and PORT env variable

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from os import getenv
 from dotenv_vault import load_dotenv
 load_dotenv()
 
 from eirb_com_helper.server import app 
 
+HOST = getenv("HOST", "0.0.0.0")
+PORT = int(getenv("PORT", 5000))
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host=HOST, port=PORT)
